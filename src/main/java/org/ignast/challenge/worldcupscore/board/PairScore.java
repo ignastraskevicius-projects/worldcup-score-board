d77b2c6de401c675f70efb324aecfb6d70ab4ac3
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-05-11 12:37:53 +0300

    publicly visible PairScore DTO knows how to convert back to Game domain object

--- src/main/java/org/ignast/challenge/worldcupscore/board/PairScore.java
+++ src/main/java/org/ignast/challenge/worldcupscore/board/PairScore.java
@@ -1,8 +1,19 @@
 package org.ignast.challenge.worldcupscore.board;
 
 import lombok.NonNull;
+import lombok.val;
 
 public record PairScore(@NonNull Home homeTeam, int homeScore, @NonNull Away awayTeam, int awayScore) {
+    static PairScore fromGame(final Game game) {
+        val participants = game.getParticipants();
+        return new PairScore(
+            participants.homeTeam(),
+            game.getHomeScore(),
+            participants.awayTeam(),
+            game.getAwayScore()
+        );
+    }
+
     Game toGame() {
         return new Game(new Participants(homeTeam, awayTeam), homeScore, awayScore);
     }
